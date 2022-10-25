fff925b55fa1b241c7b1acfcd70b37a1bba8faea
Hyang6450 <s1422@naver.com>
2022-10-25 21:11:28 +0900

    chapter1-출력

---
+++ src/main/java/j01_출력/PrintTest1.java
@@ -0,0 +1,28 @@
+package j01_출력;
+
+/**
+ *
+ */
+public class PrintTest1 {
+    public static void main(String[] args) {
+        // 한 줄 주석
+        /*
+        여러줄 주석
+        */
+
+        /**
+         *
+         * 클래스 또는 메소드의 역할을 명시 또는 설명하기 위한 주석
+         */
+
+        // 문자열 출력
+        System.out.print("이름: ");
+        System.out.println("김지향");
+        System.out.print("주소: ");
+        System.out.println("부산 동구 범일동");
+
+        boolean isUsernameCheckFlag = true;
+
+
+    }
+}
